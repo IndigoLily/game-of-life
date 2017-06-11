09b7f94aeb94e037cc49e50a08fe1473deef54ee
Indigo Lily <yulex.42@gmail.com>
2017-06-11 07:55:36 -0300

    Added isAlive function, and now the simulation runs

--- sketch.js
+++ sketch.js
@@ -1,19 +1,57 @@
 var grid = [];
-const dim = 32;
-const mult = 10;
+const dim = 128;
+const mult = 3;
 
 function drawGrid() {
   for( let y = 0; y < dim; y++ ) {
     for( let x = 0; x < dim; x++ ) {
-      if( grid[y][x] === 1 ) {
+      if( grid[y][x] ) {
         rect( x*mult, y*mult, mult, mult );
       }
     }
   }
 }
 
-function isAlive( cell ) {
+function isAlive( x, y ) {
+  var nbrs = 0;
+  var cell = grid[y][x];
   
+  for( let i = -1; i <= 1; i++ ) {
+    // skip if out of bounds
+    if( y+i < 0 || y+i >= dim ) {
+      continue;
+    }
+    
+    for( let j = -1; j <= 1; j++ ) {
+      // skip if out of bounds
+      if( x+j < 0 || x+j >= dim ) {
+        continue;
+      }
+      // skip if center
+      if( i === 0 && j === 0 ) {
+        continue;
+      }
+      
+      if( grid[y+i][x+j] ) {
+        nbrs++;
+      }
+    }
+  }
+  
+  // TODO: simplify into single statement
+  if( cell ) {
+    if( nbrs < 2 ) {
+      return false;
+    } else if( nbrs <= 3 ) {
+      return true;
+    } else if( nbrs > 3 ) {
+      return false;
+    }
+  } else {
+    if( nbrs === 3 ) {
+      return true;
+    }
+  }
 }
 
 function stepGrid() {
@@ -22,7 +60,7 @@ function stepGrid() {
   for( let y = 0; y < dim; y++ ) {
     localGrid[y] = [];
     for( let x = 0; x < dim; x++ ) {
-      localGrid[y][x] = isAlive( grid[y][x] );
+      localGrid[y][x] = isAlive( x, y );
     }
   }
   
@@ -39,15 +77,16 @@ function setup() {
   for( let y = 0; y < dim; y++ ) {
     grid[y] = [];
     for( let x = 0; x < dim; x++ ) {
-      grid[y][x] = ( Math.random() > .9 ) ? 1 : 0;
+      grid[y][x] = ( Math.random() > .9 );
     }
-    console.log(grid[y]);
   }
   
   drawGrid();
+  frameRate(20);
 }
 
 function draw() {
-  // grid = stepGrid();
-  // drawGrid();
+  background( 200 );
+  grid = stepGrid();
+  drawGrid();
 }
